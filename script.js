7a8e0a85b53c98cf7fa978b40beca0434c152ec9
Starboy756 <taseentariq5@gmail.com>
2025-06-26 18:29:52 +0500

    added new files

---
+++ script.js
@@ -0,0 +1,23 @@
+// window.alert(`Ayo slamalaikum`)
+
+
+// const userName = "taseen";
+// let gpa = 3.8;
+// let online = true;
+// let isStudent = true;
+
+// console.log(`Your GPA is: ${gpa}`)
+// console.log(`your name is ${userName}`)
+// console.log(`bro is online ${online}`)
+
+// document.getElementById("p1").textContent = `Your GPA is: ${gpa}`;
+// document.getElementById("p2").textContent = `Your online Status is: ${online}`;
+// document.getElementById("p3").textContent = `Your Student Status is: ${isStudent}`;
+
+// let x = Math.pow(30,3);
+// console.log(x);
+
+
+let userName;
+userName = window.prompt("Whats Your Name?");
+console.log(userName);
