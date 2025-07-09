a67379ec9f5cb56274e21aced4e63ab75b5ed491
Starboy756 <taseentariq5@gmail.com>
2025-07-09 22:37:52 +0500

    made a new temp converter by myself FIRSTT TIMEEEs LESS GOOO

---
+++ Temperature Converter/script.js
@@ -0,0 +1,15 @@
+const submit = document.getElementById("mySubmit");
+const radioCtof = document.getElementById("ctof");
+const radioFtoc = document.getElementById("ftoc");
+const topInput = document.getElementById("input");
+const lowerH1 = document.getElementById("answer");
+
+submit.onclick = function(){
+    if(radioCtof.checked){
+        lowerH1.textContent = `${(topInput.value * 9/5) + 32} °F`;
+    } else if (radioFtoc.checked){
+        lowerH1.textContent = `${(topInput.value - 32) * 5/9} °C`;
+    } else {
+        lowerH1.textContent = `Select Something Valid`;
+    }
+};
